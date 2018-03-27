3f8db348674f90663919a176c2811e685deed6b7
Fhelicia Selas <fhelicia@flixy-games.com>
2018-03-27 16:50:08 +0800

    Matrix Search Operations

--- search_matrix.cc
+++ search_matrix.cc
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<vector>
 #include<sstream>
-
+#include<algorithm>
 
 using namespace std;
 
@@ -18,137 +18,217 @@ string decrypt(string raw_text)
 
 	return output;
 }
-void listAvailableCommands(){
-cout<<endl;
-cout<<"Available Commands are: "<<endl;
-cout<<endl;
-cout<<"1. searchSequence "<<endl<<"2. searchUnordered "<<endl<<"3. searchMaxMatch "<<endl<<"4. exit "<<endl;
-cout<<endl;
-cout<<"usage example: searchSequence 1 2 3"<<endl;
-
+void printMatrix(vector< vector<int> > matrix){
+    
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
-void searchSequence(vector<int> tosearch){
+void listAvailableCommands(){
+    cout<<endl;
+    cout<<endl<<"Available Commands are: "<<endl;
+    cout<<endl;
+    cout<<"1. searchSequence "<<endl<<"2. searchUnordered "<<endl<<"3. searchMaxMatch "<<endl<<"4. exit "<<endl;
+    cout<<endl;
+    cout<<"usage example: searchSequence 1 2 3"<<endl;
+}
+
+void searchSequence(vector<int> tosearch,vector< vector <int> > matrix){
 
 	cout<<"Need to search sequence: ";
 	for(int i=0;i<tosearch.size();i++)
 			cout<<tosearch[i]<<" ";
 	cout<<endl;
-	cout<<"Yet to be implemented"<<endl;
+    cout<<"Matrix to be searched: "<<endl;
+    printMatrix(matrix);
+    bool found = false;
+    for(int i=0;i<matrix.size();i++){
+       
+        if(search(matrix[i].begin(), matrix[i].end(), tosearch.begin(), tosearch.end()) != (matrix[i].end())){
+                    cout<<"Sequence found in row: "<<i<<endl; //Assuming row ordering starts from 0
+                    found = true;
+            }
+    }
+    if(!found)
+        cout<<"Sequence not found in matrix"<<endl;
 
 }
-void searchUnordered(vector<int> tosearch){
+
+void searchUnordered(vector<int> tosearch,vector< vector <int> > matrix){
 
 	cout<<"Need to search unordered: ";
 	for(int i=0;i<tosearch.size();i++)
 			cout<<tosearch[i]<<" ";
 	cout<<endl;
-	cout<<"Yet to be implemented"<<endl;
-
+    cout<<"Matrix to be searched: "<<endl;
+    printMatrix(matrix);
+    for(int i=0;i<matrix.size();i++){
+        int count=0;
+        for(int j=0;j<tosearch.size();j++){
+            if(find(matrix[i].begin(), matrix[i].end(), tosearch[j]) != (matrix[i].end())){
+                ++count;
+            }
+           
+        
+        }
+        if(count == tosearch.size()){
+            cout<<"Sequence found in row: "<<i<<endl; //Assuming row ordering starts from 0
+        }
+    }
+	
 }
-void searchMaxMatch(vector<int> tosearch){
+
+void searchMaxMatch(vector<int> tosearch,vector< vector <int> > matrix){
 
 	cout<<"Need to search maximum matching row for: ";
 	for(int i=0;i<tosearch.size();i++)
 			cout<<tosearch[i]<<" ";
+    
 	cout<<endl;
-	cout<<"Yet to be implemented"<<endl;
+    
+    cout<<"Matrix to be searched: "<<endl;
+    printMatrix(matrix);
+    int max = -1,maxrow = -1;
+    for(int i=0;i<matrix.size();i++){
+        int count=0;
+        for(int j=0;j<tosearch.size();j++){
+            if(find(matrix[i].begin(), matrix[i].end(), tosearch[j]) != (matrix[i].end())){
+                ++count;
+            }
+            
+            
+        }
+        if(count > max){
+            max = count;
+            maxrow = i;
+        }
+    }
+    
+    if(maxrow > -1)
+        cout<<"Maximum matches found in row: "<<maxrow<<endl;
 
 }
+void printRunCommand(){
+    cout<<endl<<"Please specify input file name"<<endl;
+    cout<<endl<<"Run as ./search.o test.txt"<<endl;
+}
+
+
+vector< vector<int> > openAndParseData(char **argv,vector< vector<int> > &matrix ){
+    
+    ifstream input_file;
+    string rawdata,parsed;
+    input_file.open(argv[1]); //opening the input file
+    
+    if(input_file.is_open()){
+        
+        while(getline(input_file,rawdata)){
+            
+            parsed = decrypt(rawdata); //decrypting the data row by row
+            vector<int> row;
+            istringstream iss(parsed);
+            for(string s; iss >> s;){
+                int x=0;
+                stringstream ss(s);
+                ss >> x;
+                row.push_back(x); //getting values of the row
+            }
+            matrix.push_back(row); // storing in matrix row by row
+        }
+    }
+    
+    input_file.close();
+    
+    return matrix;
+}
+
+void parseCommandInput(string &cmd,vector<int> &tosearch,string line){
+    
+    
+        
+        int found=0,pos=0;
+        bool first = false;
+        while((found = line.find_first_of(' ',pos))!= string::npos){
+            
+            if(!first){
+                cmd = line.substr(pos,found - pos);
+                first = true;
+                
+            }
+            else
+            {
+                stringstream tmp(line.substr(pos,found - pos));
+                int x = 0;
+                tmp >> x;
+                tosearch.push_back(x);
+            }
+            pos = found+1;
+        }
+        if(!first){
+            cmd = line.substr(pos);
+        }
+        else{
+            stringstream tmp(line.substr(pos));
+            int x = 0;
+            tmp >> x;
+            tosearch.push_back(x);
+        }
+    
+}
+
+
 int main(int argc, char **argv){
 
-	vector<vector<int>> matrix;
+	vector< vector<int> > matrix;
 
 	if(argc < 2)
 	{	
-		cout<<"Please specify input file name"<<endl;
+        printRunCommand();
 		return -1;
 	}
+    
 	listAvailableCommands();
-	ifstream input_file;
-	ofstream ofile("decrypted.txt");
-	input_file.open(argv[1]);
-	string rawdata;
-	int row_count = 0, col_count = 0;
-	string parsed;
-	if(input_file.is_open()){
 	
-
-		while(getline(input_file,rawdata)){	
-			parsed = decrypt(rawdata);
-			//ofile<<parsed<<endl;
-			vector<int> row;
-			istringstream iss(parsed);
-			for(string s; iss >> s;){
-				int x=0;
-				stringstream ss(s);
-				ss >> x;
-				row.push_back(x);			
-			}
-			matrix.push_back(row);
-		}
-	}
+    openAndParseData(argv,matrix);
 	
-	/*for(int i=0;i<matrix.size();i++){
-		for(int j=0;j<matrix[i].size();j++){
-			cout<<matrix[i][j]<<" ";
-		}
-		cout<<endl;
-	}*/
-
-	bool exitcmd = false;
-	while(!exitcmd){
-		string cmd,line;
-		vector<int> tosearch;
-		cout<<"Enter command: "<<endl;
-		getline(cin,line);
+    bool exitcmd = false; //has exit command been entered
+    string cmd;
+    
+    while(!exitcmd){
+        
+        string cmd,line;
+        vector<int> tosearch;
+        
+        cout<<endl<<"Enter command: "<<endl;
+        getline(cin,line);
+        
+        parseCommandInput(cmd,tosearch,line);
 	
-		int found=0,pos=0;
-		bool first = false;
-		while((found = line.find_first_of(' ',pos))!= string::npos){
-
-			if(!first){
-				cmd = line.substr(pos,found - pos);
-				first = true;
-
-			}
-			else
-			{	
-				stringstream tmp(line.substr(pos,found - pos));
-				int x = 0;
-				tmp >> x;
-				tosearch.push_back(x);
-			}
-			pos = found+1;
-		}
-		if(!first){
-			cmd = line.substr(pos);
-		}
-		else{
-		stringstream tmp(line.substr(pos));
-		int x = 0;
-		tmp >> x;
-		tosearch.push_back(x);
-		}
-		if(tosearch.size()==0)
-		{
-			cout<<"Please input integers to search"<<endl;
-			listAvailableCommands();
-		}
-		else
-		{
-			if(cmd == "searchSequence")
-				searchSequence(tosearch);
-			else if(cmd == "searchUnordered")
-				searchUnordered(tosearch);
+        // validateInput(cmd,tosearch); -- To Do. validate if integer.
+        
+        if(tosearch.size()==0 && cmd !="exit")
+        {
+            cout<<"Please input integers to search"<<endl;
+            listAvailableCommands();
+        }
+        else{
+            if(cmd =="exit"){
+                exitcmd = true;
+            }
+            else if(cmd == "searchSequence")
+                searchSequence(tosearch,matrix);
 			else if(cmd =="searchMaxMatch")
-				searchMaxMatch(tosearch);
-			else if(cmd =="exit"){
-				exitcmd = true;
-		
+				searchMaxMatch(tosearch,matrix);
+            else if(cmd == "searchUnordered")
+                searchUnordered(tosearch,matrix);
 			else{
-			cout<<"Command Not Found"<<endl;
-			listAvailableCommands();
+                cout<<endl<<"ERROR: Command Not Found"<<endl;
+                listAvailableCommands();
 			}
-		}
+        }
+		
 	}
 }
